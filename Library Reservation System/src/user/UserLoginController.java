3dfb73715f5607b718b91fc4fe887472c232c879
ndb796 <ndb796@DESKTOP-4T2VKN8>
2018-02-07 20:00:19 +0900

    기초 데이터베이스 세팅

---
+++ Library Reservation System/src/user/UserLoginController.java
@@ -0,0 +1,16 @@
+package user;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class UserLoginController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+
+	}
+
+}
