851ec2de42090ca210ecadcac7c674644e1a77ea
david <david@100.100.108.11>
2018-02-07 20:35:38 +0900

    회원가입 정보입력 ui

--- Library Reservation System/src/util/DatabaseUtil.java
+++ Library Reservation System/src/util/DatabaseUtil.java
@@ -12,7 +12,7 @@ public class DatabaseUtil {
 			/* 자신의 데이터베이스 아이디 및 비밀번호에 맞게 변경하기 */
 			String dbURL = "jdbc:mysql://localhost:3306/LRS?characterEncoding=utf8&verifyServerCertificate=false&useSSL=true";
 			String dbID = "root";
-			String dbPassword = "root";
+			String dbPassword = "1234";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 			
